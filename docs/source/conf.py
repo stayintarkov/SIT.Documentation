c1645834286317a932d5ff1c0d7652948a3e370e
trippyone <137233897+trippyone@users.noreply.github.com>
2024-02-24 10:11:23 -0500

    Change config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Stay In Tarkov'
+copyright = '2024, Paulov'
+author = 'Paulov'
 
 release = '0.1'
 version = '0.1.0'
